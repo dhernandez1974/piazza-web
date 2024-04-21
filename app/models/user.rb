02973b9f87a55099011fc13e899c8cc7f5eeb204
dhernandez1974 <163759124+dhernandez1974@users.noreply.github.com>
2024-04-21 09:47:49 -0500

    “Set up user, organization, and membership models”

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  normalizes :name, with: -> (name) { name.strip.capitalize }
+  normalizes :email, with: -> (email) { email.strip.downcase }
+
+  has_secure_password
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
 end
